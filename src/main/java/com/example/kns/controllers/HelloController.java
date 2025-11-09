72441e0211c853e786d539b44a7cc31ea9b4d8d7
Rūta <rutagaiz@gmail.com>
2025-11-09 16:04:36 +0200

    Added dependencies and created intergration/unit tests
    Required dependancies were added for mockito and zonky embedded postgres
    db. Created HelloService and HelloController tests. Waiting for task
    ESFFF2-6 (repo and model) to proceed with embedded integration
    test setup.

--- src/main/java/com/example/kns/controllers/HelloController.java
+++ src/main/java/com/example/kns/controllers/HelloController.java
@@ -2,12 +2,24 @@ package com.example.kns.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.kns.services.HelloService;
 
 @RestController
 public class HelloController {
 
+	private final HelloService service;
+
+	public HelloController(HelloService service) {
+		this.service = service;
+	}
+
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello from Spring Boot!";
+	}
+
+	@GetMapping("/test")
+	public String greeting() {
+		return service.greet();
 	}
 }
---
+++ src/main/java/com/example/kns/services/HelloService.java
@@ -0,0 +1,10 @@
+package com.example.kns.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+	public String greet() {
+		return "approval";
+	}
+}
