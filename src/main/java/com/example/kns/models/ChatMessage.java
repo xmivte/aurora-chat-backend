cf380ebc49429921dc0869b308f6950a25d24225
Rūta <rutagaiz@gmail.com>
2025-11-17 18:58:17 +0200

    WIP

--- src/main/java/com/example/kns/models/ChatMessage.java
+++ src/main/java/com/example/kns/models/ChatMessage.java
@@ -1,4 +1,15 @@
 package com.example.kns.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+    private Date createdAt;
 }
--- src/main/java/com/example/kns/repositories/ChatMessagesMapper.java
+++ src/main/java/com/example/kns/repositories/ChatMessagesMapper.java
@@ -1,4 +1,17 @@
 package com.example.kns.repositories;
 
-public class ChatMessagesMapper {
+import com.example.kns.models.ChatMessage;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface ChatMessagesMapper {
+
+    @Insert("INSERT INTO chat_messages(sender, content) VALUES (#{sender}, #{content})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(ChatMessage message);
+
+    @Select("SELECT * FROM chat_messages ORDER BY created_at ASC LIMIT #{limit}")
+    List<ChatMessage> findLastMessages(@Param("limit") int limit);
 }
