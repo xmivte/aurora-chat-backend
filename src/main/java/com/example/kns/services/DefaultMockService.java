7b2196954137cce05de00af732c5e622b22ab8ec
Rūta <rutagaiz@gmail.com>
2025-11-17 13:04:22 +0200

    Syntax issues addressed

--- src/main/java/com/example/kns/services/DefaultMockService.java
+++ src/main/java/com/example/kns/services/DefaultMockService.java
@@ -3,7 +3,7 @@ package com.example.kns.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MockServiceImpl implements MockService {
+public class DefaultMockService implements MockService {
     @Override
     public String getText(){
         return "aa";
