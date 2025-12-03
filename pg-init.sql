6d4def2fce152603f344288abcd0733c5261e7c2
rutagaiz <rutagaiz@gmail.com>
2025-12-03 19:12:41 +0200

    Modified pg-init.sql to give db_user full access

--- pg-init.sql
+++ pg-init.sql
@@ -3,3 +3,4 @@ CREATE SCHEMA db;
 CREATE USER db_user WITH PASSWORD 'password';
 ALTER ROLE db_user SET search_path = db;
 GRANT ALL ON SCHEMA db TO db_user;
+GRANT CREATE ON DATABASE postgres TO db_user;
